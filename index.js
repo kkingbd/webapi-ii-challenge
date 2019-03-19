6a4f09d552223846e07b28b0ba8dd9366f8bfeec
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-03-19 15:18:43 -0400

    Post, delete and put enpoints added

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ server.use(express.json());
 server.get("/api/posts", (req, res) => {
     db
         .find()
-		.then(posts => {
-			res.status(200).json({ posts });
+		.then(post => {
+			res.status(200).json({ post });
 		})
 		.catch(() => {
 			res.status(500).json({ error: "The posts information could not be retrieved." });
@@ -29,6 +29,85 @@ server.get("/api/posts/:id", (req, res) => {
 		.catch(() => {
 			res.status(500).json({ error: "The post information could not be retrieved." });
 		});
+});
+
+server.post("/api/posts", (req, res) => {
+	const post = req.body;
+	db.insert(post)
+		.then(result => {
+			if (post) {
+                db
+                    .findById(result.id)
+					.then(post => {
+						res.status(201).json(post);
+					})
+					.catch(err =>
+						res.status(500).json({
+							errorMessage: "The POST findById failed",
+							error: err,
+						})
+					);
+			} else {
+				res.status(400).json({
+					errorMessage:
+						"Please provide title and contents for the post.",
+				});
+			}
+		})
+		.catch(err =>
+			res.status(500).json({
+				error:
+					"There was an error while saving the post to the database",
+			})
+		);
+});
+server.delete("/api/posts/:id", (req, res) => {
+	const id = req.params.id;
+	db.findById(id)
+		.then(post => {
+			if (post) {
+				db.remove(id).then(() => {
+					res.status(200).json(post);
+				});
+			} else {
+				res.status(404).json({
+					message: "The post with the specified ID does not exist.",
+				});
+			}
+		})
+		.catch(err =>
+			res.status(500).json({ error: "The post could not be removed" })
+		);
+});
+server.put("/api/posts/:id", (req, res) => {
+	const id = req.params.id;
+	const postUpdates = req.body;
+	db.update(id, postUpdates)
+		.then(result => {
+			if (postUpdates && postUpdates.contents) {
+				db.findById(result.id)
+					.then(post => {
+						console.log(postUpdates);
+						res.status(201).json(postUpdates);
+					})
+					.catch(err =>
+						res.status(500).json({
+							errorMessage: "The PUT findById failed",
+							error: err,
+						})
+					);
+			} else {
+				res.status(400).json({
+					errorMessage:
+						"Please provide title and contents for the post.",
+				});
+			}
+		})
+		.catch(err =>
+			res.status(500).json({
+				error: "The post information could not be modified." ,
+			})
+		);
 });
 
 server.listen(PORT, () => {
