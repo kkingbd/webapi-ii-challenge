0918957fb30012bfba44b5a6e6a9436ab1d1857e
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-03-19 17:24:00 -0400

    bug fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,45 @@
-import React, { Component } from 'react';
-import {Route, NavLink} from 'react-router-dom';
-import './App.css';
-import axios from 'axios';
-import Posts from './components/Posts';
-
-const url = new URL('http://localhost:4000/api/posts/')
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: []
-    }
+	constructor() {
+		super();
+		this.state = {
+			posts: [],
+		};
+	}
+
+	componentDidMount() {
+		axios
+			.get("http://localhost:4000/api/posts/")
+			.then(res => {
+        const posts = Array.isArray(res.data.post)? res.data.post :[];
+				console.log("Server Response :", res);
+        this.setState({ posts});
+			})
+			.catch(err => {
+        console.log("Server Error: ", err);
+
+			});
   }
 
-  componentDidMount() {
-    axios
-    .get(url)
-    .then(res => {
-      this.setState({
-        posts: res.data
-      })
-    })
-    .catch(err => console.log(err))
-  }
-
-  addPost = data => {
-    axios
-    .post(url, data)
-    .then(res => {
-      this.setState({
-        posts: res.data
-      })
-    })
-    .catch(err => console.log(err))
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <NavLink to='/api/posts'>Home</NavLink>
-        <Route exact path='/api/posts'
-        render={props => <Posts posts={this.state.posts} {...props} />}
-        />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				{this.state.posts.map(post => (
+					<div className="post-container" key={post.id}>
+						<br />
+						<h3>Post #{post.id}</h3>
+						<h4>{post.title}</h4>
+						<p>{post.contents}</p>
+						<h6>Created on {post.created_at}</h6>
+						<br />
+					</div>
+				))}
+			</div>
+		);
+	}
 }
 
 export default App;
